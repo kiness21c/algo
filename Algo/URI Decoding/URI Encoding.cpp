1459e3d0c033ffe8214bb7cd9bff39f6c9a00970
HyunJun <kiness@gmail.com>
2015-05-25 20:56:35 +0900

    Update files.

---
+++ Algo/URI Decoding/URI Encoding.cpp
@@ -0,0 +1,56 @@
+
+
+
+#include <string>
+#include <map>
+
+using namespace std;
+
+
+void init_codes(map<string, char>& code)
+{
+	code.clear();
+
+	code("%20", ' ');
+	code("%21", '!');
+	code("%24", '$');
+	code("%25", '%');
+	code("%28", '(');
+	code("%29", ')');
+	code("%2a", '*');
+}
+
+
+
+
+
+int main()
+{
+
+	map<string, char> codes;
+	// file input for test.
+	freopen("sample_input.txt", "r", stdin);
+
+	int data_N;
+	c
+
+	cin >> data_N;
+	for (int test_case = 0; i < data_N; ++i)
+	{
+		cin >> str_tmp;
+	}
+	
+
+	
+	init_codes
+
+
+	
+
+
+
+
+
+
+	return 0;
+}
